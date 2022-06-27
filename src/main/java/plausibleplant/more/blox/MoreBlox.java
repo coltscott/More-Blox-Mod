6287a2a81307867cbd373d78a08d4275501a703a
coltscott <coltonscott0@yahoo.com>
2022-06-27 17:54:34 -0400

    Finishing touches on brix blocks

--- src/main/java/plausibleplant/more/blox/MoreBlox.java
+++ src/main/java/plausibleplant/more/blox/MoreBlox.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -18,8 +19,13 @@ public class MoreBlox implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("moreblox");
     
     public static final Item BRIX = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
-	public static final Block BRIX_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
-		.strength(0.2f));
+	public static final Block BRIX_BLOCK = new Block(FabricBlockSettings.of(Material.STONE)
+		.strength(2.0f, 6.0f)
+		.sounds(BlockSoundGroup.NETHER_BRICKS)
+		.requiresTool());
+	public static final Block DARK_CLAY = new Block(FabricBlockSettings.of(Material.SOIL)
+		.strength(0.6f)
+		.sounds(BlockSoundGroup.GRAVEL));
 
 	@Override
 	public void onInitialize() {
@@ -29,10 +35,11 @@ public class MoreBlox implements ModInitializer {
 		//Brix block registry
 
 		Registry.register(Registry.BLOCK, new Identifier("moreblox", "brix_block"), BRIX_BLOCK);
-		Registry.register(Registry.ITEM, new Identifier("moreblox", "brix_block"), new BlockItem(BRIX_BLOCK, new FabricItemSettings().group(ItemGroup.MATERIALS)));
+		Registry.register(Registry.ITEM, new Identifier("moreblox", "brix_block"), new BlockItem(BRIX_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		//Dark clay registry
 
-		//Registry.register(Registry.BLOCK, new Identifier("moreblox", "dark_clay"), DARK_CLAY);
+		Registry.register(Registry.BLOCK, new Identifier("moreblox", "dark_clay"), DARK_CLAY);
+		Registry.register(Registry.ITEM, new Identifier("moreblox", "dark_clay"), new BlockItem(DARK_CLAY, new FabricItemSettings().group(ItemGroup.MATERIALS)));
 
 
 		LOGGER.info("More Blox launched");
